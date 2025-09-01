ecb5ef63ddc2dd7d4d27aaa7318365d9f30324cc
Syed Zaheer <70836665+syedmzaheer@users.noreply.github.com>
2025-09-02 03:20:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #Import standard dependencies
 import cv2
 import os
+import uuid
 import random
 import numpy as np
 from matplotlib import pyplot as plt
@@ -58,3 +59,32 @@ while cap.isOpened():
 
     #Collect positives
     if cv2.waitKey(1) & 0xFF == ord('p'):
+        imgname = os.path.join(POS_PATH, '{}.jpg'.format(uuid.uuid1()))
+        #Write out positive image
+        cv2.imwrite(imgname, frame)
+
+    #Show the image back to the screen
+    cv2.imshow('Image Collection', frame)
+
+    #Break gracefully
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+#Release the webcam
+cap.release()
+cv2.destroyAllWindows()
+
+plt.imshow(frame[120:120+250, 200:200+250, :])
+
+def data_aug(img):
+    data = []
+    for i in range(9):
+        img = tf.image.stateless_random_brightness(img, max_delta=0.02, seed=(1,2))
+        img = tf.image.stateless_random_contrast(img, lower=0.6, upper=1, seed=(1,3))
+        #img = tf.image.stateless_randon_crop(img, size=(20,20,3), seed=(1,2))
+        img= tf.image.stateless_random_flip_left_right(img, seed=(np.random.randint(100), np.random.randit(100)))
+        img = tf.image.stateless_random_jpeg_quality(img, min_jpeg_quality=90, max_jpeg_quality=100, seed=(np.random.randint(100), np.random.randint(100)))
+        img = tf.image.stateless_random_saturation(img, lower=0.9, upper=1, seed=(np.random.randint(100), np.random.randint(100)))
+        
+        data.append(img)
+    return data
